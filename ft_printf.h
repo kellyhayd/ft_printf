85708587b1d1b2fa694d74d3604d70d5d027a17e
Kelly Rocha Hayd <krocha-h@c2r6p2.42sp.org.br>
2023-12-04 16:36:21 -0300

    reorg

--- ft_printf.h
+++ ft_printf.h
@@ -1,13 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 15:02:00 by krocha-h          #+#    #+#             */
+/*   Updated: 2023/12/04 16:25:05 by krocha-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 #ifndef FT_PRINTF_H
 # define FT_PRINT_H
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdarg.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
 
 int		ft_printf(const char *str, ...);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putchar_fd(char c, int fd);
+int		print_digit(char *base, int num);
+int		print_numhex_up(va_list ap);
+int		print_numhex_low(va_list ap);
+int		print_ptrhex(va_list ap);
+
 
 #endif
--- main.c
+++ main.c
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/04 15:02:05 by krocha-h          #+#    #+#             */
+/*   Updated: 2023/12/04 15:02:06 by krocha-h         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 
 // #include "ft_printf.h"
 #include <stdio.h>
@@ -8,7 +20,7 @@ int main(void)
 	int		i;
 	float	j;
 	double	k;
-	
+
 	// c = 'k';
 	i = 420;
 	j = 42.5;
@@ -19,7 +31,7 @@ int main(void)
 	printf("Printf imprime float: %f\n", j);
 
 	printf("Printf imprime decimal: %d\n", i);
-	
+
 	// printf("Printf imprime ")
 
 	return (0);
