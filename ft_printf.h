8cf354f93a11ca14001abee33d32fa39089b6074
Kelly Rocha Hayd <krocha-h@c2r6p2.42sp.org.br>
2023-12-04 17:09:50 -0300

    reorg

--- ft_printf.h
+++ ft_printf.h
@@ -6,10 +6,9 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:02:00 by krocha-h          #+#    #+#             */
-/*   Updated: 2023/12/04 16:25:05 by krocha-h         ###   ########.fr       */
+/*   Updated: 2023/12/04 16:41:15 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #ifndef FT_PRINTF_H
 # define FT_PRINT_H
@@ -25,6 +24,5 @@ int		print_digit(char *base, int num);
 int		print_numhex_up(va_list ap);
 int		print_numhex_low(va_list ap);
 int		print_ptrhex(va_list ap);
-
 
 #endif
