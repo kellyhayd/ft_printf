a2add6eeaf39d738418b73ac6c026291f187db1f
Kelly Rocha Hayd <krocha-h@c2r4p2.42sp.org.br>
2023-12-05 17:35:28 -0300

    header modified

--- ft_printf.h
+++ ft_printf.h
@@ -6,23 +6,28 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:02:00 by krocha-h          #+#    #+#             */
-/*   Updated: 2023/12/04 16:41:15 by krocha-h         ###   ########.fr       */
+/*   Updated: 2023/12/05 17:19:02 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_PRINTF_H
-# define FT_PRINT_H
+# define FT_PRINTF_H
 
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
 
 int		ft_printf(const char *str, ...);
-void	ft_putstr_fd(char *s, int fd);
-void	ft_putchar_fd(char c, int fd);
+int		id_type(char c, va_list ap);
+int		print_char(char c);
+int		print_str(char *str);
 int		print_digit(char *base, int num);
-int		print_numhex_up(va_list ap);
-int		print_numhex_low(va_list ap);
-int		print_ptrhex(va_list ap);
+int		print_numhex_up(long int num);
+int		print_numhex_low(long int num);
+int		print_ptrhex(long int num);
+int		print_dec_int(int num);
+int		print_undec(unsigned int num);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char *s, int fd);
 
 #endif
