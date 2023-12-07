468a7a776e03bee50b5eccad2554585251899cec
Kelly Rocha Hayd <krocha-h@c2r5p2.42sp.org.br>
2023-12-07 14:43:14 -0300

    main tests int

--- main.c
+++ main.c
@@ -6,30 +6,39 @@
 /*   By: krocha-h <krocha-h@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/04 15:02:05 by krocha-h          #+#    #+#             */
-/*   Updated: 2023/12/05 17:11:24 by krocha-h         ###   ########.fr       */
+/*   Updated: 2023/12/07 14:14:24 by krocha-h         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-// #include "ft_printf.h"
+#include "ft_printf.h"
 #include <stdio.h>
 
 int main(void)
 {
-	char	c;
-	// int		i;
-	// float	j;
-	// double	k;
-
-	c = 'k';
-	// i = 420;
-	// j = 42.5;
-	// k = 420;
-	ft_printf("ft_printf imprime char: %c Kelly\n", c);
-	printf("Printf imprime int: %c\n", c);
+	// char	c;
+	int		i;
+	int		j;
+	int		k;
+	int		n1;
+	int		n2;
 
 
-	// printf("Printf imprime ")
+	// c = 'k';
+	i = 420;
+	j = -42;
+	k = 420000;
+
+	// ft_printf("ft_printf imprime char: %c Kelly\n", c);
+	// printf("Printf imprime int: %c\n", c);
+
+	n1 = ft_printf("ft_printf imprime int: %d\n", i);
+	n2 = printf("___Printf imprime int: %d\n", i);
+	printf("ft = %d\nprintf = %d\n", n1, n2);
+	// ft_printf("ft_printf imprime int: %cdKelly\n", j);
+	// printf("Printf imprime int: %d\n", j);
+	// ft_printf("ft_printf imprime int: %cdKelly\n", k);
+	// printf("Printf imprime int: %d\n", k);
 
 	return (0);
 }
